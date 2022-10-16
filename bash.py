0fd5d4018362f7c4cc25f11dcf4450fbdc8113bf
Herobrine <91737914+Herobrine5292@users.noreply.github.com>
2022-10-16 12:58:37 +0530

    Add files via upload

---
+++ bash.py
@@ -0,0 +1,15 @@
+import os
+# apt update && apt full-upgrade
+# pkg install python 
+# pkg install git
+# apt update && apt full-upgrade
+# pip install wheel
+# pkg update
+# pkg install build-essential
+# pkg install binutils
+# pip install coincurve --no-binary all
+# pip install bit
+# MATHLIB="m" pip3 install numpy
+# pip install bitcoinlib --no-binary all
+# pip install eth_hash
+os.system("apt update && apt full-upgrade \n pkg install git \n apt update && apt full-upgrade \n pip install wheel \n pkg update \n pkg install build-essential\n pkg install binutils\n pip install coincurve --no-binary all\n pip install bit\n MATHLIB="m" pip3 install numpy\n pip install bitcoinlib --no-binary all\n pip install eth_hash\n ")
