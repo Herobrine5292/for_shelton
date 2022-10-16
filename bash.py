9755423b55832f5fd16ad91da108f6ae58bb1704
Herobrine <91737914+Herobrine5292@users.noreply.github.com>
2022-10-16 13:13:14 +0530

    gh

--- bash.py
+++ bash.py
@@ -1,2 +1,2 @@
 import os
-os.system("apt update && apt full-upgrade -Y\npip install wheel\npkg update\npkg install build-essential -Y\npkg install binutils -Y\npip install coincurve --no-binary all\npip install bit\nMATHLIB=\"m\" pip3 install numpy\npip install bitcoinlib --no-binary all\npip install eth_hash")
+os.system("pip install --upgrade pip\napt update && apt full-upgrade -Y\npip install wheel\npkg update\npkg install build-essential -Y\npkg install binutils -Y\npip install coincurve --no-binary all\npip install bit\nMATHLIB=\"m\" pip3 install numpy\npip install bitcoinlib --no-binary all\npip install eth_hash")
