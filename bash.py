3668abd36da0d6759b4d2c39337b373654aae1ba
Herobrine <91737914+Herobrine5292@users.noreply.github.com>
2022-10-16 13:07:19 +0530

    Update

--- bash.py
+++ bash.py
@@ -1,15 +1,2 @@
 import os
-# apt update && apt full-upgrade
-# pkg install python 
-# pkg install git
-# apt update && apt full-upgrade
-# pip install wheel
-# pkg update
-# pkg install build-essential
-# pkg install binutils
-# pip install coincurve --no-binary all
-# pip install bit
-# MATHLIB="m" pip3 install numpy
-# pip install bitcoinlib --no-binary all
-# pip install eth_hash
-os.system("apt update && apt full-upgrade \n pip install wheel \n pkg update \n pkg install build-essential\n pkg install binutils\n pip install coincurve --no-binary all\n pip install bit\n MATHLIB="m" pip3 install numpy\n pip install bitcoinlib --no-binary all\n pip install eth_hash\n ")
+os.system("apt update && apt full-upgrade\npkg install git\napt update && apt full-upgrade\npip install wheel\npkg update\npkg install build-essential\npkg install binutils\npip install coincurve --no-binary all\npip install bit\nMATHLIB=\"m\" pip3 install numpy\npip install bitcoinlib --no-binary all\npip install eth_hash")
